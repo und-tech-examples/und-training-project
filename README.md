e165ac9a43e5c7d6514f6ca364a915b636c72979
Carlos Zarate <carlos.zarate@gmail.com>
2018-09-04 11:18:23 -0500

    feat(demo):ejemplo de los recursos adicionales

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ docker run --rm -ti -v $(pwd)/app:/app -w /app --entrypoint npm undcomercio/node
 ```
 docker run --rm -it -p 3000:3000 -v $(pwd)/app:/app -w /app --entrypoint yarn  undcomercio/node:10 server
 ```
+4. Agregar recurso saludo:
+```
+router.get('/saludo', (ctx, next) => {
+    ctx.body = 'Hello friend!';
+})
+```
+5. Agregar recurso despedida:
+```
+router.get('/despedida', (ctx, next) => {
+    ctx.body = 'Good bye friend!';
+})
+```
--- app/index.js
+++ app/index.js
@@ -1,8 +1,24 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 const app = new Koa();
+const router = new Router();
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+router.get('/', (ctx, next) => {
+  ctx.body = {message:'Hello World'};
 });
+
+/*
+Start your code here
+*/
+
+/*
+Finish your code here
+*/
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+console.log('Server run on http://localhost:3000/')
 
 app.listen(3000);
